45084fc325702c7e88fd7f6d35132c38d54797eb
ichtrojan <ordrizzy@gmail.com>
2019-11-18 03:09:44 +0100

    OTPs table Migration

---
+++ src/database/migrations/2019_05_11_000000_create_otps_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOtpsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('otps', function (Blueprint $table) {
+            $table->increments('id')->index();
+            $table->string('identifier');
+            $table->integer('token');
+            $table->integer('validity');
+            $table->boolean('valid')->default(true);;
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('otps');
+    }
+}
