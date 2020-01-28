12b21d5ea01128b0e5c4fe3242f7c9331b42b1f9
ichtrojan <ordrizzy@gmail.com>
2020-01-28 16:16:10 +0100

    ⚠️ God be with us

--- src/OtpServiceProvider.php
+++ src/OtpServiceProvider.php
@@ -22,6 +22,14 @@ class OtpServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__ . '/Database/migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+        $this->publishMigrations();
+    }
+
+    protected function publishMigrations()
+    {
+        $this->publishes([
+             __DIR__ . "/database/migrations/2019_05_11_000000_create_otps_table.php" => database_path('migrations/' . date("Y_m_d_His", time()) . '_create_otps_table.php'),
+        ], 'laravel-otp-migration');
     }
 }
