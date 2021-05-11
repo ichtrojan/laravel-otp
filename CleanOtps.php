8bea341698ea0beda270ff6a53e6e4428f308172
Bharat Dhaker <bharatkumardhaker@gmail.com>
2021-05-11 15:08:23 +0530

    Add files via upload

---
+++ CleanOtps.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Ichtrojan\Otp\Otp;
+use Illuminate\Console\Command;
+
+class CleanOtps extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'otp:clean';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Clean Otp database, remove all old otps that is expired or used.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        try {
+            $otps = Otp::where('valid', 0)->count();
+
+            $this->info("Found {$otps} expired otps.");
+            Otp::where('valid', 0)->delete();
+            $this->info("Removed successfully expired  tokens!");
+        } catch (\Exception $e) {
+            $this->error("Error:: {$e->getMessage()}");
+        }
+        return 0;
+    }
+}
