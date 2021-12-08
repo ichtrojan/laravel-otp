7f7d56ae4596660260b1ac932d3960a8c01606d9
Alfred Okedi <okedialf@gmail.com>
2021-12-08 08:13:29 +0300

    fix issue#13 by implementing static method generatePin

--- src/Otp.php
+++ src/Otp.php
@@ -26,13 +26,13 @@ class Otp extends Facade
     {
         Model::where('identifier', $identifier)->where('valid', true)->delete();
 
-        $token = str_pad($this->generatePin(), 4, '0', STR_PAD_LEFT);
+        $token = str_pad(self::generatePin(), 4, '0', STR_PAD_LEFT);
 
         if ($digits == 5)
-            $token = str_pad($this->generatePin(5), 5, '0', STR_PAD_LEFT);
+            $token = str_pad(self::generatePin(5), 5, '0', STR_PAD_LEFT);
 
         if ($digits == 6)
-            $token = str_pad($this->generatePin(6), 6, '0', STR_PAD_LEFT);
+            $token = str_pad(self::generatePin(6), 6, '0', STR_PAD_LEFT);
 
         Model::create([
             'identifier' => $identifier,
@@ -97,7 +97,7 @@ class Otp extends Facade
      * @param int $digits
      * @return string
      */
-    private function generatePin($digits = 4)
+    private static function generatePin($digits = 4)
     {
         $i = 0;
         $pin = "";
